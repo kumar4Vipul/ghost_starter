331c90d7e11febe88effed8d830962e16f788ddd
kumar4vipul <vk44vipulkumar1998@gmail.com>
2018-04-16 04:05:06 +0530

    first commit

--- app/src/main/java/com/google/engedu/ghost/TrieNode.java
+++ app/src/main/java/com/google/engedu/ghost/TrieNode.java
@@ -15,85 +15,97 @@
 
 package com.google.engedu.ghost;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
 
 public class TrieNode {
-    private HashMap<String, TrieNode> children;
+    private HashMap<Character, TrieNode> children;
     private boolean isWord;
+    Random rand =new Random();
 
     public TrieNode() {
         children = new HashMap<>();
         isWord = false;
     }
-
-    public void add(String s) {add(s, 0);}
-
-    private void add(String s, int position) {
-        if (position >= s.length()) return;
-
-        char c = s.charAt(position);
-        TrieNode n = children.get(c);
-
-        if (n == null) {
-            n = new TrieNode();
-            children.put(String.valueOf(c), n);
+    public void add(String s) {
+        HashMap<Character, TrieNode> temp = children;
+        for (int i = 0; i < s.length(); i++){
+            if (!temp.containsKey(s.charAt(i))){
+                temp.put(s.charAt(i), new TrieNode());
+            }
+            if (i == s.length() - 1){
+                temp.get(s.charAt(i)).isWord = true;
+            }
+            temp = temp.get(s.charAt(i)).children;
         }
-
-        if (position == s.length() - 1) {
-            n.isWord = true;
-        }
-
-        n.add(s, position + 1);
     }
 
     public boolean isWord(String s) {
-        return isWord(s, 0);
+        TrieNode temp = searchNode(s);
+        if (temp == null)
+            return false;
+        else
+            return temp.isWord;
     }
 
-    public boolean isWord(String s, int position) {
-        if (position >= s.length())
-            return false;
-
-        char c = s.charAt(position);
-        TrieNode n = children.get(c);
-
-        if (n == null) return false;
-
-        if (position == s.length() - 1) {
-            return n.isWord;
+    private TrieNode searchNode(String s){
+        TrieNode temp = this;
+        for (int i = 0; i < s.length(); i++){
+            if (!temp.children.containsKey(s.charAt(i))){
+                return null;
+            }
+            temp = temp.children.get(s.charAt(i));
         }
-
-        return n.isWord(s, position + 1);
+        return temp;
     }
 
     public String getAnyWordStartingWith(String s) {
-        return getAnyWordStartingWith(s, 0);
-    }
-
-    public String getAnyWordStartingWith(String s, int position) {
-        if (position >= s.length()) {
-            if (children.size() == 0)
-                return null;
-
-            Random r = new Random();
-            Object[] chars = children.keySet().toArray();
-
-            return s + chars[r.nextInt(chars.length)];
+        TrieNode temp = searchNode(s);
+        if (temp == null){
+            return null;
         }
-
-        char c = s.charAt(position);
-        TrieNode n = children.get(c);
-
-        if (n == null) return null;
-
-        return n.getAnyWordStartingWith(s, position + 1);
+        while (!temp.isWord){
+            for (Character c: temp.children.keySet()){
+                temp = temp.children.get(c);
+                s += c;
+                break;
+            }
+        }
+        return s;
     }
 
     public String getGoodWordStartingWith(String s) {
-        return null;
+        TrieNode temp = searchNode(s);
+        if (temp == null){
+            return null;
+        }
+        // get a random word
+        ArrayList<Character> charsNoWord = new ArrayList<>();
+        ArrayList<Character> charsWord = new ArrayList<>();
+        Character c;
+
+        while (true){
+            charsNoWord.clear();
+            charsWord.clear();
+            for (Character ch: temp.children.keySet()){
+                if (temp.children.get(ch).isWord){
+                    charsWord.add(ch);
+                } else {
+                    charsNoWord.add(ch);
+                }
+            }
+            if (charsNoWord.size() == 0){
+                s += charsWord.get( rand.nextInt(charsWord.size()) );
+                break;
+            } else {
+                c = charsNoWord.get( rand.nextInt(charsNoWord.size()) );
+                s += c;
+                temp = temp.children.get(c);
+            }
+        }
+        return s;
     }
 }
-
 
